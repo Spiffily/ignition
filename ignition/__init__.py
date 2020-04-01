218fd0b7495fa0249a2e647e49856502904e444a
ElSenor64 <sillynoodle36@gmail.com>
2020-04-01 10:37:58 -0600

    More work on running from snap. Icon coming soon.

---
+++ ignition/__init__.py
@@ -0,0 +1,15 @@
+__all__ = ['Igntition']
+
+__productname__ = 'Ignition'
+# Expecting trailing "-rcN" or "" for stable releases.
+__version__     = "0.0.3"
+__copyright__   = "Copyright 2020 Josh L & contributors"
+__author__      = "Josh L"
+__author_email__= "sillynoodle36@gmail.com"
+__description__ = "Hit the ground running on your fresh Ubuntu installation."
+__license__  = "Licensed under the GNU GPL v3 or any later version"
+
+#from offlineimap.error import OfflineImapError
+# put this last, so we don't run into circular dependencies using
+# e.g. offlineimap.__version__.
+from ignition.init import Igntition
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
    name='ignition',
-   version='0.0.2',
+   version='0.0.3',
    description='Hit the ground running on your fresh Ubuntu installation.',
    license="GPL 3+",
    long_description="The Linux world is full of apps. You could be looking for an app that does something, only to find that there are literally hundreds of options. Ignition will show you common apps for certain categories and explain their little nuances in an easy format. Just choose the ones to install, and Ignition will start the engine.",
@@ -13,7 +13,7 @@ setup(
 #    author_email='foomail@foo.com',
 #    url="http://www.foopackage.com/",
    packages=['ignition'],  #same as name
-   install_requires=['figlet'], #external packages as dependencies
+   install_requires=[], #external packages as dependencies
 #    scripts=[
 #             'scripts/cool',
 #             'scripts/skype',
