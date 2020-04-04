3bc270c21a9b3691efcb94db2c726d8de3ae4d51
ElSenor64 <sillynoodle36@gmail.com>
2020-04-04 09:51:35 -0600

    Added icon stuff and App name in shell.

--- ignition/ignition.py
+++ ignition/ignition.py
@@ -16,7 +16,8 @@ class HomeWindow(Gtk.ApplicationWindow):
         Gtk.Window.__init__(self, title="Ignition")
         # system("mkdir ~/.local/share/ignition/")
         # self.appdata = home+"/.local/share/ignition/"
-
+        self.set_wmclass ("Ignition", "Ignition")
+        self.set_icon_from_file('/home/josh/ignition/Armature.png')
         installer = Install()
 
         tabs = Gtk.Notebook.new()
